37b4dabeddd1545c2da349e1d2c28b7ad9c84226
Virginia <7017118+VirginiaDooley@users.noreply.github.com>
2024-12-03 10:12:26 +0000

    Define js constants

---
+++ script.js
@@ -0,0 +1,6 @@
+const taskInput = document.getElementById('taskInput');
+const addTaskButton = document.getElementById('addTaskButton');
+const taskList = document.getElementById('taskList');
+const progressBar = document.getElementById('progressBar');
+const progressText = document.getElementById('progressText');
+
